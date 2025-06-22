f84fc4beedc42dbdd4b95cd31029e41cc0bcd628
Josué-Ramos1 <ramosloezajosue@outlook.com>
2025-06-22 17:00:44 -0600

    nuevo conf

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://Josue-Ramos1.github.io',
-  	base: 'Notas',
+  	base: '/Notas',
 	integrations: [
 		starlight({
 			title: 'Mis Notas',
